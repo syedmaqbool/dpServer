be7f47b773b955cb3c239403a4e638b7dad25006
syedmaqbool <syedmaqbool@users.noreply.github.com>
2023-01-30 21:46:10 +0500

    Update db.config.ts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm"
 
 export const myDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
+   type: "mysql",
+    host: "sql7.freemysqlhosting.net",
     port: 3306,
-    username: "root",
-    password: "",
-    database: "parking_db",
+    username: "sql7594440",
+    password: "5pdNLpnVKV",
+    database: "sql7594440",
     entities: ["src/entity/*.ts"],
     logging: true,
     synchronize: false,
+    connectTimeout:100000
 })
